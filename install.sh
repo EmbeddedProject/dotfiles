8c0a6023a902ad3ae95b0f2bb3e6f160ad7e7986
Robin Jarry <robin.jarry@6wind.com>
2014-10-22 14:01:02 +0200

    install.sh: add verbose output
    Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+echo "dotfiles root: $(pwd)"
 rel_root=$(python -c "import os.path; print os.path.relpath('$(pwd)', '$HOME')")
 
 for file in bashrc bash_aliases inputrc vim vimrc gitconfig gitfiles hgrc hgfiles
 do
-	ln -sf $rel_root/$file ~/.$file
+	ln -sfv $rel_root/$file ~/.$file
 done
 
 mkdir -p ~/bin
 for script in acked-by ctags-global ctags-local
 do
-	ln -sf ../$rel_root/bin/$script ~/bin/$script
+	ln -sfv ../$rel_root/bin/$script ~/bin/$script
 done
