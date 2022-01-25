66768c61f3e433fa6fe218af21f8cb597e06b4f6
Robin Jarry <robin@jarry.cc>
2022-01-25 09:58:44 +0100

    polybar: use clementine for music
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- config/polybar/music.py
+++ config/polybar/music.py
@@ -88,7 +88,7 @@ if args.play_pause is not None:
 try:
     session_bus = dbus.SessionBus()
     spotify_bus = session_bus.get_object(
-        'org.mpris.MediaPlayer2.spotify',
+        'org.mpris.MediaPlayer2.clementine',
         '/org/mpris/MediaPlayer2'
     )
 
