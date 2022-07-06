EmbeddedProject/dotfiles

commit fd88086309d89148ca9e6a4c62043a89aef582a9

mk: add uninstall command
Signed-off-by: Robin Jarry <robin@jarry.cc>

Makefile
@@ -18,6 +18,10 @@ help:
 install: dotfiles scripts
 	@:
 
+.PHONY: uninstall
+uninstall: uninstall-dotfiles uninstall-scripts
+	@:
+
 xdg_config := $(shell find config/ -type f)
 xdg_local := $(shell find local/ -type f)
 
@@ -59,6 +63,10 @@ $(HOME)/.%: %
 	@ln -srvf $< $@
 	@! [ $(@D) = $(HOME)/.gnupg ] || chmod -c 600 $< $@
 
+.PHONY: uninstall-dotfiles
+uninstall-dotfiles:
+	@$(foreach f,$(addprefix $(HOME)/.,$(DOTFILES)), rm -fv $f;)
+
 BINFILES = $(wildcard bin/*)
 
 .PHONY: scripts
@@ -69,6 +77,10 @@ $(HOME)/bin/%: bin/%
 	@! [ -e $@ ] || rm -rf -- $@
 	@mkdir -pv $(@D)
 	@ln -srvf $< $@
+
+.PHONY: uninstall-scripts
+uninstall-scripts:
+	@$(foreach f,$(addprefix $(HOME)/,$(BINFILES)), echo rm -fv $f;)
 
 DEBS += brightnessctl
 DEBS += clementine

commit d1eab3461fadc5159e80a9527626ce6d2012beb3

mk: fix uninstall bin files
Fixes: fd88086309d8 ("mk: add uninstall command")
Signed-off-by: Robin Jarry <robin@jarry.cc>

Makefile
@@ -81,7 +81,7 @@ $(HOME)/bin/%: bin/%
 
 .PHONY: uninstall-scripts
 uninstall-scripts:
-	@$(foreach f,$(addprefix $(HOME)/,$(BINFILES)), echo rm -fv $f;)
+	@$(foreach f,$(addprefix $(HOME)/,$(BINFILES)), rm -fv $f;)
 
 DEBS += brightnessctl
 DEBS += clementine